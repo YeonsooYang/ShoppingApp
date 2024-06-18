9d642d9c581a75829bba71695ac725b24ba1c2ea
yeonsoo <yysoo412412@gmail.com>
2024-06-18 15:53:55 +0900

    modified:   client/src/App.tsx 	modified:   server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
-const Child = ({state,action}:any) => {
-  return <button type="button" onClick={action}>{state}</button>
-}
+const Child = ({state}:any) => {
+  return <button type="button">{state}</button>
+};
 
 const App = () => {
-  const [number,setNumber]=useState(0);
-  const increase = () => setNumber(number+1);
+  const [number1,setNumber1]=useState(0);
+  const [number2,setNumber2]=useState(0);
+
+  const HandleChange = () => {
+    setNumber1((prev)=>prev+2);
+    setNumber2((prev)=>prev+3);
+  };
 
   return(
-    <Child state={number} action={increase}/>
+    <div>
+      <Child state={number1} />
+      <Child state={number2} />
+      <button onClick={HandleChange}>Change</button>
+    </div>
   );
-}
+};
 
 export default App;
